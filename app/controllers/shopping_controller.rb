9a753e662524fad5b86dc902d32504338217702d
mike2611 <mpuentes2611@gmail.com>
2022-02-01 17:20:28 -0600

    Add items to shopping list

--- app/controllers/shopping_controller.rb
+++ app/controllers/shopping_controller.rb
@@ -1,4 +1,22 @@
 class ShoppingController < ApplicationController
   def index
+    inventory_foods = InventoryFood.all
+    recipes_foods = RecipeFood.all
+    food_recipes = {}
+    food_invetory = {}
+    @missing_food = {}
+    recipes_foods.each do |f|
+      food_recipes[f.food_id] = f.quantity
+    end
+    inventory_foods.each do |f|
+      food_invetory[f.food_id] = f.quantity
+    end
+    food_recipes.each do |key, value|
+      if(!food_invetory.key?(key)) 
+        @missing_food[Food.find_by(id: key)] = value
+      elsif(food_invetory.key?(key) && value > food_invetory[key])
+        @missing_food[Food.find_by(id: key)] = value - food_invetory[key]
+      end
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,15 @@
 #     
 #   Food.create(name: 'apple', measurement_unit: 'grs', price: 10, user_id: 1)
 #   Food.create(name: 'bannana', measurement_unit: 'grs', price: 5, user_id: 1)
+#   Food.create(name: 'sugar', measurement_unit: 'grs', price: 5, user_id: 1)
 #   Recipe.create(name: 'smoothie', preparation_time: 4, cooking_time: 0, description: 'smoothie apple bannana', public:true, user_id: 1 )
 #   RecipeFood.create(quantity: 2, recipe_id:1, food_id:1)
 #   RecipeFood.create(quantity: 1, recipe_id:1, food_id:2)
-# 
+#   RecipeFood.create(quantity: 1, recipe_id:1, food_id:3)
+#   Inventory.create(name: 'inventory for smoothie', user_id: 1)
+#   InventoryFood.create(quantity: 1, food_id:1, inventory_id: 1)
+#   InventoryFood.create(quantity: 1, food_id:2, inventory_id: 1)
+#   
 #
 #
 #
