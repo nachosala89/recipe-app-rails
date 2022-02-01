20fe054416c1ed9a5361db232c565954d25cab80
mike2611 <mpuentes2611@gmail.com>
2022-02-01 10:05:33 -0600

    Add user and food to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,10 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+#   User.create(name: 'Joe)
+#     
+#   Food.create(name: 'apple', measurement_unit: 'grs', price: 10, user_id: 1)
+#   Food.create(name: 'bannana', measurement_unit: 'grs', price: 5, user_id: 1)
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#
+#
