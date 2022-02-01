fcf2710414f1d271cbd34c80bf2f6ec1f631a93e
mike2611 <mpuentes2611@gmail.com>
2022-02-01 17:28:35 -0600

    Add amout of items to buy and food needed

--- app/controllers/shopping_controller.rb
+++ app/controllers/shopping_controller.rb
@@ -5,6 +5,7 @@ class ShoppingController < ApplicationController
     food_recipes = {}
     food_invetory = {}
     @missing_food = {}
+    @price = 0
     recipes_foods.each do |f|
       food_recipes[f.food_id] = f.quantity
     end
@@ -14,8 +15,11 @@ class ShoppingController < ApplicationController
     food_recipes.each do |key, value|
       if(!food_invetory.key?(key)) 
         @missing_food[Food.find_by(id: key)] = value
+        @price += value * Food.find_by(id: key).price
       elsif(food_invetory.key?(key) && value > food_invetory[key])
-        @missing_food[Food.find_by(id: key)] = value - food_invetory[key]
+        missing_value = value - food_invetory[key]
+        @missing_food[Food.find_by(id: key)] = missing_value
+        @price += missing_value * Food.find_by(id: key).price
       end
     end
   end
