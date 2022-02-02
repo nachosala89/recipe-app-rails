9fee56315a60b613d95bace2f7cfc3cd823d9e58
mike2611 <mpuentes2611@gmail.com>
2022-02-02 10:13:45 -0600

    Applied corrections

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   def current_user
     # return unless session[:user_id]
     # @current_user ||= User.find(session[:user_id])
-    User.find_by(id: 2)
+    User.first
   end
 end
