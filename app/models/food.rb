5c8d21cc08505476ef19b1701b37cf8f94026a43
nachosala89 <nachosala89@gmail.com>
2022-02-02 11:28:19 -0300

    Fix linters errors

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :user
-  
+
   def name_with_unit
     "#{name} (#{measurement_unit})"
   end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
-  has_many :foods, :through => :recipe_foods
+  has_many :foods, through: :recipe_foods
 
   def total_price
     price = 0
