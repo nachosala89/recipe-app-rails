8927efe43634a0e9f20eb391e321af6d0876edf1
mike2611 <mpuentes2611@gmail.com>
2022-02-03 08:05:11 -0600

    Modify shopping_controller

--- app/controllers/shopping_controller.rb
+++ app/controllers/shopping_controller.rb
@@ -1,25 +1,31 @@
 class ShoppingController < ApplicationController
   def index
-    food_recipes = current_food_recipes
-    food_invetory = current_food_invetory
-    @missing_food = {}
-    @price = 0
-
-    food_recipes.each do |key, value|
-      if !food_invetory.key?(key)
-        @missing_food[Food.find_by(id: key)] = value
-        @price += value * Food.find_by(id: key).price
-      elsif food_invetory.key?(key) && value > food_invetory[key]
-        missing_value = value - food_invetory[key]
-        @missing_food[Food.find_by(id: key)] = missing_value
-        @price += missing_value * Food.find_by(id: key).price
-      end
-    end
+    @missing_food = missing_price[0]
+    @price = missing_price[1]
   end
 
   private
 
-  def current_food_recipes
+  def missing_price
+    food_recipes = current_food_recipes
+    food_invetory = current_food_invetory
+    missing_food = {}
+    price = 0
+
+    food_recipes.each do |key, value|
+      if !food_invetory.key?(key)
+        missing_food[Food.find_by(id: key)] = value
+        price += value * Food.find_by(id: key).price
+      elsif food_invetory.key?(key) && value > food_invetory[key]
+        missing_value = value - food_invetory[key]
+        missing_food[Food.find_by(id: key)] = missing_value
+        price += missing_value * Food.find_by(id: key).price
+      end
+    end
+    [missing_food, price]
+  end
+
+  def current_food_recipes      
     recipes = Recipe.where("user_id = #{current_user.id}")
     food_recipes = {}
     recipes.each do |r|
